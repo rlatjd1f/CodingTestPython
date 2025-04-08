ff93e9ce586ea2e851af1d2d7e0d043e1dd068bb
rlatjd1f <dl290382@gmail.com>
2025-04-08 21:42:14 +0900

    2025-04-08 study commit

---
+++ sort/BKJ_3273.py
@@ -0,0 +1,17 @@
+import sys
+
+imput = sys.stdin.readline
+
+n = int(input())
+numbers = list(map(int, input().strip().split()))
+num_dict = {v: 1 for v in numbers}
+
+x = int(input())
+res_set = set()
+
+for num in numbers:
+    diff = x - num
+    if diff in num_dict:
+        res_set.add((num, diff))
+
+print(len(res_set) // 2)
